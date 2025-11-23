18f8a7cf7abdbb6050320eebbadb6008e2a7cccb
Cristian Manuel Gonzalez Blanco <cristianmgb21@gmail.com>
2025-11-23 11:57:14 -0500

    feat: Integrates Tanstack Query for data management
    Enables robust data fetching, caching, and state synchronization across the application. Configures `QueryClientProvider` at the root, making Tanstack Query's capabilities globally available. Also optimizes Vite's dependency pre-bundling for the new library.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
-import { StrictMode } from 'react';
+import { StrictMode, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from 'rick-morty-components-lib';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </StrictMode>
-);
+export function Root() {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<Root />);
